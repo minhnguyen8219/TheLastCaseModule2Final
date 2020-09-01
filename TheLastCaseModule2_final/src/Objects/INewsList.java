d07f146c522393dfddef63c83d6c950df6cb470a
Nguyễn Bùi Quang Minh <67447333+npn8219@users.noreply.github.com>
2020-09-01 08:43:48 +0700

    ver1.0

---
+++ TheLastCaseModule2_final/src/Objects/INewsList.java
@@ -0,0 +1,14 @@
+package Objects;
+
+import java.util.ArrayList;
+
+public interface INewsList {
+
+    void setList();
+    void creatNews(int id);
+    void editNews(int id);
+    void deleteNews(int id);
+    void displayList();
+    void recordList();
+
+}
